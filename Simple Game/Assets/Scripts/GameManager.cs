b2c82d4361334082f7f830e4815552974cc6de47
abdulFred <abdullaevfarhodjon@gmail.com>
2019-08-23 18:34:14 +0900

    Add changes

---
+++ Simple Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject wintText;
+    public GameObject restartBtn;
+
+    int score = 0;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ScoreUp()
+    {
+        score++;
+        if (score >= 5)
+        {
+            Win();
+        }
+    }
+
+    void Win()
+    {
+        wintText.SetActive(true);
+        restartBtn.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleGame");
+    }
+}
